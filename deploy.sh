a0509a5a3ce5aab11dc4698d9c04164c0a26a2b0
Anvith <anvithgowda2219@gmail.com>
2026-01-20 18:48:55 +0530

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+set -e
 
-IMAGE_NAME=anvith3068/node-devops-app
+DOCKER_USER=anvith3068
+IMAGE_NAME=node-devops-app
 
+echo "Stopping old container..."
 docker stop nodeapp || true
 docker rm nodeapp || true
 
-echo "Pulling latest image..."
+echo "Pulling image..."
 docker pull $DOCKER_USER/$IMAGE_NAME:latest
 
-docker run -d -p 80:3000 --name nodeapp $IMAGE_NAME:latest
+echo "Starting container..."
+docker run -d \
+  --restart unless-stopped \
+  -p 80:3000 \
+  --name nodeapp \
+  $DOCKER_USER/$IMAGE_NAME:latest
+
+echo "Deployment done"
+
 
